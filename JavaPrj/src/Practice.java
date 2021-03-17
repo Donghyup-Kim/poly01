9b75fe8c3f406e0e9350838618099d025116ac32
data17 <data17@DESKTOP-Q7316PD>
2021-03-17 09:56:09 +0900

    두번째

---
+++ JavaPrj/src/Practice.java
@@ -0,0 +1,11 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a, b = 5, c = 3;
+    a = b + c;
+    System.out.println(a);
+	}
+
+}
