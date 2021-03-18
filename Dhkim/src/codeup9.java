a1d35299a7f22abdf592836dfe84409c0a69785f
data <data@data-HP>
2021-03-18 20:32:00 +0900

    연습

---
+++ Dhkim/src/codeup9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class codeup9 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = 15;
+		a= scan.nextInt();
+		System.out.println(a);
+scan.close();
+	}
+
+}
