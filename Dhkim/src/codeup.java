d0a804eefa1e69c4112c9a4ff4744a7d59d59e98
Donghyup-Kim <donghyup56@naver.com>
2021-03-19 17:49:11 +0900

    연습

---
+++ Dhkim/src/codeup.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class codeup {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    float a;
+	    a= sc.nextFloat();
+	    System.out.printf("%06f",a);
+        sc.close();
+}
+}
