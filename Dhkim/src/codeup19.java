03539f8e90e28eaba2f472f7e2482a4c495b4c94
Donghyup-Kim <donghyup56@naver.com>
2021-03-20 16:20:03 +0900

    연습

---
+++ Dhkim/src/codeup19.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class codeup19 {
+		public static void main(String[] args) throws IOException {
+			    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String num[] = br.readLine().split("-");
+				System.out.print(num[0]+num[1]);
+			}
+		
+	}
+
