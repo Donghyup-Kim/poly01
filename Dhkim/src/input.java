a7406789d7f0efedcf930a6a40d81b64fb372696
Donghyup-Kim <donghyup56@naver.com>
2021-03-29 23:00:01 +0900

    연습

---
+++ Dhkim/src/input.java
@@ -0,0 +1,21 @@
+public class input {
+     
+    public static void main(String[] args) {
+         
+                        
+            printTwoTimes("a", "-");
+            
+            printTwoTimes("a", "*");
+            
+            printTwoTimes("a", "&");
+            printTwoTimes("b", "!");
+ 
+    }
+                                    
+    public static void printTwoTimes(String text, String delimiter) {
+        System.out.println(delimiter);
+        System.out.println(text);
+        System.out.println(text);
+    }
+ 
+}
