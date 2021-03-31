63f3ac17ea285f893e94eab40555714ff7f20347
Donghyup-Kim <donghyup56@naver.com>
2021-03-31 12:49:48 +0900

    연습

---
+++ JavaPrj/src/Exam12.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exam12 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a;
+
+		System.out.println("1~4중에 선택하세요 :");
+		a = s.nextInt();
+
+		switch (a) {
+		case 1:
+			System.out.println("1을 선택햇다");
+			break;
+
+		case 2:
+			System.out.println("2를 선택했다");
+			break;
+
+		case 3:
+			System.out.println("3을 선택했다");
+			break;
+
+		case 4:
+			System.out.println("4를 선택했다");
+			break;
+
+		default:
+			System.out.println("이상한걸 선택했다");
+
+		}
+	}
+}
