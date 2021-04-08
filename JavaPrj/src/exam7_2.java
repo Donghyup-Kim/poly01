bb39bea07a3543289184f86f7b309336ce3f7fb4
Donghyup-Kim <donghyup56@naver.com>
2021-04-08 20:30:19 +0900

    wd

---
+++ JavaPrj/src/exam7_2.java
@@ -0,0 +1,16 @@
+
+public class exam7_2 {
+
+	public static void main(String[] args) {
+		int a;
+		int hap = 0;
+		a = 0;
+		while (a < 11) {
+			hap += a;
+
+			a++;
+		}
+		System.out.printf("1에서 10까지의합 :" + hap);
+	}
+
+}
