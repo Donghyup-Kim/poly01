a138f9e89270d2c7f6505d7bb716bd0e2744118f
Donghyup-Kim <donghyup56@naver.com>
2021-04-14 08:46:55 +0900

    exam

---
+++ JavaPrj/src/dfew.java
@@ -0,0 +1,13 @@
+
+public class dfew {
+
+	public static void main(String[] args) {
+		int a;
+		for (a = 1; a < 11; a++) {
+			if (a % 3 == 0) {
+				System.out.print("3ÀÇ ¹è¼ö :");
+			}
+			System.out.println(a);}
+	}
+
+}
