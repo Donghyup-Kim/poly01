b31892a09a73729254bd0e16d3c2cbccdee0d45a
Donghyup56 <donghyup56@naver.com>
2021-04-15 19:18:31 +0900

    연습

---
+++ hayoon/src/hayoon/d_rep.java
@@ -0,0 +1,15 @@
+package hayoon;
+
+public class d_rep {
+
+	public static void main(String[] args) {
+int a;
+for(a=0;a<10;a++) {
+	System.out.println(a);
+	if(a%3==0) {
+		System.out.println("3ÀÇ ¹è¼ö");
+	}
+}
+	}
+
+}
