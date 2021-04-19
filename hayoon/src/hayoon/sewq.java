67f569eabfa5623ea4afcec8d8d7dd295d9908fb
Donghyup56 <donghyup56@naver.com>
2021-04-19 16:37:50 +0900

    k

---
+++ hayoon/src/hayoon/sewq.java
@@ -0,0 +1,31 @@
+package hayoon;
+
+
+
+import java.util.Scanner;
+
+public class sewq {
+
+   public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      int n = s.nextInt();
+      int k = 0;
+      for (int i = 0; i < 2 * n - 1; i++) {
+         if (i < n) {
+            k++;
+         } else {
+            k--;
+         }
+         for (int j = 0; j < k; j++) {
+            System.out.print("*");
+         }
+
+         System.out.println();
+      }
+   }
+
+
+
+	}
+
+
